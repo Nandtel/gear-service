a3ea6af7bad64c6149e6f71c706b5f772f6c6c80
Uhtiybr <uhtiybr90@gmail.com>
2016-11-24 21:37:23 +0300

    Update mysql-connector-java, guava, spring-context-support, poi-ooxml, jackson-datatype-hibernate4, mongo-java-driver, jsoup,  nekohtml, jackson-datatype-jsr310

--- src/main/java/com/gearservice/config/ErrorConfiguration.java
+++ src/main/java/com/gearservice/config/ErrorConfiguration.java
@@ -2,7 +2,7 @@ package com.gearservice.config;
 
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
