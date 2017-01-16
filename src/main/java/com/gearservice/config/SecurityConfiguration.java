0879b0a08d75ee796c4d41376a13d9e746ad4835
Uhtiybr <uhtiybr90@gmail.com>
2017-01-16 21:31:43 +0300

    Enhance security with referrer policy

--- src/main/java/com/gearservice/config/SecurityConfiguration.java
+++ src/main/java/com/gearservice/config/SecurityConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
@@ -121,8 +122,8 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterAfter(csrfHeaderFilter(), SessionManagementFilter.class)
                 .addFilterBefore(new ReCaptchaAuthFilter(reCaptchaProperties), BasicAuthenticationFilter.class)
-                .headers()
-                .contentSecurityPolicy("default-src https: 'self'; object-src 'none'; script-src 'self' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:");
+                .headers().contentSecurityPolicy("default-src https: 'self'; object-src 'none'; script-src 'self' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:")
+                .and().addHeaderWriter(new StaticHeadersWriter("Referrer-Policy", "same-origin"));
     }
 
     /**
