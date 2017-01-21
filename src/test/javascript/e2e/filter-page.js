5c1d1292e94d5bf408a02b926f5237ed6906f544
Uhtiybr <uhtiybr90@gmail.com>
2017-01-21 22:11:03 +0300

    Add e2e tests

--- src/test/javascript/e2e/filter-page.js
+++ src/test/javascript/e2e/filter-page.js
@@ -10,13 +10,13 @@ describe('checking functionality of filter page', function() {
         {name: 'IntroducedFrom', type: 'date'},
         {name: 'IntroducedTo', type: 'date'},
         {name: 'id', type: 'input'},
-        {name: 'nameOfCustomer', type: 'autocomplete'},
-        {name: 'nameOfProduct', type: 'autocomplete'},
-        {name: 'model', type: 'autocomplete'},
+        {name: 'customerName', type: 'autocomplete'},
+        {name: 'productName', type: 'autocomplete'},
+        {name: 'modelName', type: 'autocomplete', field: 'model'},
         {name: 'serialNumber', type: 'autocomplete'},
-        {name: 'purchaserName', type: 'autocomplete'},
-        {name: 'inspectorName', type: 'autocomplete'},
-        {name: 'masterName', type: 'autocomplete'},
+        {name: 'representativeName', type: 'autocomplete'},
+        {name: 'secretary.fullname', type: 'autocomplete', field: 'secretary'},
+        {name: 'engineer.fullname', type: 'autocomplete', field: 'engineer'},
         {name: 'warrantyStatus', type: 'select', column: 'warrantyDate'},
         {name: 'readyStatus', type: 'select', column: 'readyDate'},
         {name: 'returnedToClientStatus', type: 'select', column: 'returnedToClientDate'},
@@ -26,6 +26,59 @@ describe('checking functionality of filter page', function() {
 
     var testingFilterBy = function(name, type) {
         switch(type) {
+
+            case 'input':
+                it('should filter by ' + name, function() {
+                    element(by.id('find')).click();
+                    browser.waitForAngular();
+
+                    element(by.repeater('cheque in $root.page.content').row(0).column('cheque.' + name))
+                        .getText()
+                        .then(function(sample) {
+                            element(by.model('$root.filter.' + name)).sendKeys(sample);
+
+                            element(by.id('find')).click();
+                            browser.waitForAngular();
+
+                            expect(element(by.repeater('cheque in $root.page.content').row(0).column('cheque.' + name))
+                                .getText())
+                                .toEqual(sample);
+                        });
+                });
+                break;
+
+            case 'autocomplete':
+                var field = !!arguments[3] ? arguments[3] : name;
+
+                it('should use autocomplete to fill filter field named ' + name + ' and check that every items in column ' +
+                    'equal to filter value', function() {
+                    element(by.id('find')).click();
+                    browser.waitForAngular();
+
+                    element(by.repeater('cheque in $root.page.content').row(0).column('cheque.' + name))
+                        .getText()
+                        .then(function (sample) {
+                            element(by.xpath('//md-autocomplete[@md-search-text="$root.filter.' + field + '"]'))
+                                .element(by.model('$mdAutocompleteCtrl.scope.searchText'))
+                                .sendKeys(sample.substring(0, 10));
+
+                            browser.waitForAngular();
+                            $$('[md-virtual-repeat="item in $mdAutocompleteCtrl.matches"]').first().click();
+                            element(by.id('find')).click();
+
+                            browser.waitForAngular();
+                            element.all(by.repeater('cheque in $root.page.content').column('cheque.' + name))
+                                .getText()
+                                .then(function (list) {
+                                    list.forEach(function (item) {
+                                        expect(item).toEqual(sample);
+                                    })
+                                });
+                        });
+                });
+
+                break;
+
             case 'select':
                 var column = arguments[2];
 
@@ -91,7 +144,7 @@ describe('checking functionality of filter page', function() {
     });
 
     filters.forEach(function(item) {
-        testingFilterBy(item.name, item.type, item.column);
+        testingFilterBy(item.name, item.type, item.column, item.field);
     });
 
 });
